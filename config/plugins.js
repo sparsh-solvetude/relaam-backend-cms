a632a27657840037a75502d8c03e50f6b56046b9
sparsh-solvetude <sparsh.solvetude@gmail.com>
2025-08-14 21:33:39 +0530

    Image size upload

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,18 @@
-module.exports = () => ({});
+module.exports = () => ({
+  upload: {
+    config: {
+      actionOptions: {
+        upload: {
+          sizeLimit: 20 * 1024 * 1024,
+          allowedTypes: [
+            "image/jpeg",
+            "image/png",
+            "image/webp",
+            "image/gif",
+            "image/svg+xml",
+          ],
+        },
+      },
+    },
+  },
+});
